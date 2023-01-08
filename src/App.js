0a647bdb0fd85a75555da374d07bfa542d1efcec
Ryo <7890bask@gmail.com>
2023-01-09 08:42:40 +0900

    CallComponent

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import './App.css';
 import Todo from './components/Pages/Todo/Todo';
+import { TaskProvider } from './store/Context/TaskContext/TaskContext';
 
 function App() {
   return (
-    <div style={{margin: "2em"}}>
-      <Todo/>
+    <div style={{ margin: '2em' }}>
+      <TaskProvider>
+        <Todo />
+      </TaskProvider>
     </div>
   );
 }
