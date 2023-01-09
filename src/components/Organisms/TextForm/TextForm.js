42fa2adf5c13454df242de5de0bd8e1ed389dd1f
Ryo <7890bask@gmail.com>
2023-01-09 21:42:27 +0900

    追加ロジックを実装

--- src/components/Organisms/TextForm/TextForm.js
+++ src/components/Organisms/TextForm/TextForm.js
@@ -1,16 +1,30 @@
-import React from 'react';
+import React, { useState } from 'react';
 import PropTypes from 'prop-types';
 import styles from './TextForm.module.css';
 import TextField from '../../Atoms/TextField/TextField';
 import Button from '../../Atoms/Button/Button';
+import { useTaskContext } from '../../../store/Context/TaskContext/TaskContext';
 
-const TextForm = () => (
-  <div className={styles.TextForm}>
-    <TextField />
-    <Button children='追加' className='add' />
-    <Button children='キャンセル' className='cancel' />
-  </div>
-);
+const TextForm = () => {
+  const { dispatch } = useTaskContext();
+  const [value, setValue] = useState('');
+  const handleChange = (e) => {
+    setValue(e.target.value);
+  };
+  return (
+    <div className={styles.TextForm}>
+      <TextField onChange={handleChange} />
+      <Button
+        children='追加'
+        className='add'
+        onClick={() => {
+          dispatch({ type: 'ADD', context: value });
+        }}
+      />
+      <Button children='キャンセル' className='cancel' />
+    </div>
+  );
+};
 
 TextForm.propTypes = {};
 
