f84f6b98ce3003891fa6aef54d9a689f2e83b61b
Ryo <7890bask@gmail.com>
2023-01-12 07:52:53 +0900

    props名を修正

--- src/components/Molecules/Card/Card.js
+++ src/components/Molecules/Card/Card.js
@@ -7,50 +7,41 @@ import Text from '../../Atoms/Text/Text';
 import TextInput from '../../Atoms/TextInput/TextInput';
 import { useTaskContext } from '../../../store/Context/TaskContext/TaskContext';
 // 渡ってきたPropsを振り分ける
-const Card = () => {
-  const { state, dispatch } = useTaskContext();
+const Card = (props) => {
+  const { dispatch } = useTaskContext();
+  const { todo } = props;
   const [inputMode, setInputMode] = useState(false);
   const handleChange = () => {
     setInputMode((e) => !e);
   };
   return (
     <div className={styles.Card}>
-      {state.map((tasks, index) => {
-        return (
-          <div key={index}>
-            {inputMode && (
-              <>
-                <TextInput />
-                <Button children='保存' className='save' />
-                <Button children='キャンセル' className='edit' />
-              </>
-            )}
+      {inputMode && (
+        <>
+          <TextInput />
+          <Button children='保存' className='save' />
+          <Button children='キャンセル' className='edit' />
+        </>
+      )}
 
-            {!inputMode && (
-              <>
-                <Title children={tasks.task} />
-                <Text />
-                <Button
-                  children='修正'
-                  className='edit'
-                  onClick={handleChange}
-                />
-                <Button
-                  children='削除'
-                  className='delete'
-                  onClick={() => {
-                    dispatch({
-                      type: 'DELETE',
-                      context: tasks.task,
-                      id: tasks.id,
-                    });
-                  }}
-                />
-              </>
-            )}
-          </div>
-        );
-      })}
+      {!inputMode && (
+        <>
+          <Title children={todo.task} />
+          <Text />
+          <Button children='修正' className='edit' onClick={handleChange} />
+          <Button
+            children='削除'
+            className='delete'
+            onClick={() => {
+              dispatch({
+                type: 'DELETE',
+                context: todo.task,
+                id: todo.id,
+              });
+            }}
+          />
+        </>
+      )}
     </div>
   );
 };
